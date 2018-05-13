3d6d0ef0ea8e562dc2d8980584a0a27f72d828d2
pratap <sripratap.93@gmail.com>
2018-05-13 18:08:35 +0530

    first commi

---
+++ calckivy.py
@@ -0,0 +1,19 @@
+import kivy
+kivy.require('1.10.0')
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+class calcGridLayout(GridLayout):
+    def calculate(self, calculation):
+        if calculation:
+            try:
+                self.display.text=str(eval(calculation))
+            except:
+                self.display.text = "Error"
+class calculatorApp(App):
+    def build(self):
+        return calcGridLayout()
+
+calcApp = calculatorApp()
+calcApp.run()
